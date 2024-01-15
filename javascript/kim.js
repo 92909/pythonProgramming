004328e4212c9f955086fefe4ee23d89429f1bd6
92909 <120147431+92909@users.noreply.github.com>
2024-01-15 18:51:45 +0300

    Add files via upload

---
+++ javascript/kim.js
@@ -0,0 +1,6 @@
+// File: helloworld.js
+
+// This line will print "Hello, World!" to the console
+console.log("Hello, World!");
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
